76363124e6303f7f96d29e13a804729828e4a223
tirtoga <rahulhaque07@gmail.com>
2018-02-20 17:52:45 +0600

    AlbumList component added #1

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList'
 
 export default class App extends React.Component {
     render() {
         return (
             <View>
-                <Header headerText={'Albums!'}/>
+                <Header headerText={'Bum Bums!'}/>
+                <AlbumList/>
             </View>
         );
     }
---
+++ src/components/AlbumList.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import axios from 'axios';
+
+class AlbumList extends Component {
+    state = {
+        album: []
+    };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState(
+                {
+                    album: response.data,
+                }
+            ));
+    }
+
+    // http://rallycoding.herokuapp.com/api/music_albums
+    render() {
+        console.log(this.state);
+
+        return (
+            <View>
+                <Text>Album List!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
